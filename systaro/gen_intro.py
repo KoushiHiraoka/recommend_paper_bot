b2c33afd95159e9c3c73e629d4ad6c6421ab1e13
KoushiHiraoka <hiraoka.koushi@arakawa-lab.com>
2025-05-03 17:22:08 +0900

    upgrade ui

--- systaro/gen_intro.py
+++ systaro/gen_intro.py
@@ -9,18 +9,46 @@ def summarize(paper_info):
     chat = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[{ "role":"system",
-                    "content": ("あなたは分野初心者をアシスタントするフレンドリーな先生です"
-                               "以下の形式でSlack向けにMarkdown装飾を使って出力してください。\n\n"
-                                "*今日のおすすめ論文*\n"
-                                "*タイトル*：<タイトル>\n"
-                                "*概要*：<説明>\n"
-                                "*出版年*：<出版年>\n"
-                                "*会議*：<会議名>\n"
-                                "*URL*：<URL>\n\n"
-                                "上のテンプレートを使い、最後に要旨を簡潔かつ親しみやすい口調でまとめてください。"
+                    # "content": ("あなたは分野初心者をアシスタントするフレンドリーな先生です"
+                    #            "以下の形式でSlack向けにMarkdown装飾を使って出力してください。\n\n"
+                    #             "*今日のおすすめ論文*\n"
+                    #             "*タイトル*：<タイトル>\n"
+                    #             "*概要*：<説明>\n"
+                    #             "*出版年*：<出版年>\n"
+                    #             "*会議*：<会議名>\n"
+                    #             "*URL*：<URL>\n\n"
+                    #             "上のテンプレートを使い、最後に要旨を簡潔かつ親しみやすい口調でまとめてください。"
+                    "content":("あなたは分野初心者をサポートするフレンドリーな先生です。"
+                    "これから渡す論文のタイトル、要旨、出版年、会議、URLを読み取り、"
+                    "要旨をSlackのBlock Kitで使いやすいように「本文テキストだけ」を"
+                    "親しみやすい口調で短くまとめてください。"
             )},
                    { "role":"user",
                      "content":f"タイトル: {paper_info['title']}\n\n要旨: {paper_info['abstract']}\n\n出版年: {paper_info['year']}\n\n会議: {paper_info['venue']}\n\n会議: {paper_info['url']}\n\n---\nこの論文のAbstractをわかりやすくフレンドリーな感じで説明して"}]
+    )
+    return chat.choices[0].message.content
+
+def gen_title(paper_info):
+    client = OpenAI()
+    chat = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{ "role":"system",
+                    # "content": ("あなたは分野初心者をアシスタントするフレンドリーな先生です"
+                    #            "以下の形式でSlack向けにMarkdown装飾を使って出力してください。\n\n"
+                    #             "*今日のおすすめ論文*\n"
+                    #             "*タイトル*：<タイトル>\n"
+                    #             "*概要*：<説明>\n"
+                    #             "*出版年*：<出版年>\n"
+                    #             "*会議*：<会議名>\n"
+                    #             "*URL*：<URL>\n\n"
+                    #             "上のテンプレートを使い、最後に要旨を簡潔かつ親しみやすい口調でまとめてください。"
+                    "content":("あなたは分野初心者をサポートするフレンドリーな先生です。"
+                    "これから渡す論文のタイトル、要旨を読み取り、"
+                    "要旨をSlackのBlock Kitで使いやすいように「タイトルだけ」を"
+                    "コピーライティングを意識してキャッチーなタイトルをつけてください。"
+            )},
+                   { "role":"user",
+                     "content":f"タイトル: {paper_info['title']}\n\n要旨: {paper_info['abstract']}\n\n---\nコピーライティングを意識してキャッチーなタイトルをつけてください。"}]
     )
     return chat.choices[0].message.content
 
--- systaro/test_code/to_slackbot_test.py
+++ systaro/test_code/to_slackbot_test.py
@@ -2,10 +2,15 @@ import os
 import datetime
 import requests
 import time
+import sys
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 from dotenv import load_dotenv
 load_dotenv()
+
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
 
 import gen_intro as gen 
 import search_paper as sp
@@ -15,13 +20,13 @@ import search_paper as sp
 #     return gen.summarize(selected_paper)
 
 def post_daily_paper(keyword, venue):
+    # gpt_summary = get_summary(keyword, venue)
     client = WebClient(token=os.getenv("SLACK_BOT_TOKEN"))
     channel=os.getenv("SLACK_CHANNEL")
 
+    #block kit test
     selected_paper = sp.research_paper(keyword, venue)
-    
-    gpt_summary = gen.summarize(keyword, venue)
-    pop_title = gen.gen_title(selected_paper)
+    # pop_title = gen.gen_title(selected_paper)
 
     title    = selected_paper.get("title")
     year     = selected_paper.get("year")
@@ -52,8 +57,15 @@ def post_daily_paper(keyword, venue):
             "type": "header",
             "text": {
                 "type": "plain_text",
-                "text": f"{pop_title}",
+                "text": "これはtextです",
                 "emoji": True
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"*タイトル*\n<{url}|{title}>\n"
             }
         },
         {
@@ -73,8 +85,7 @@ def post_daily_paper(keyword, venue):
             "type": "section",
             "text": {
                 "type": "mrkdwn",
-                "text": f"*どんな論文🧐？*\n{gpt_summary}", 
-                "emoji": True
+                "text": f"*どんな論文🧐？*\n hello"
             }
         }
     ]
@@ -97,10 +108,66 @@ def post_daily_paper(keyword, venue):
     # except SlackApiError as err:
     #     print("Failed to post to Slack:", err)
 
+def check_ui():
+    client = WebClient(token=os.getenv("SLACK_BOT_TOKEN"))
+    channel=os.getenv("SLACK_CHANNEL")
+
+    blocks = [
+        {
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": "これはtextです",
+                "emoji": True
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"*タイトル*\n"
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"*出版年*\n<year>\n"
+            }
+        },        
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text":
+                    f"*タイトル*\n<url>\n"
+                    f"*出版年*\n<year>\n"
+                    f"*会議*\n<venue>\n"
+                    f"*引用数*\n<citation>\n"
+                    f"*著者*\n<authors>"
+            }
+        },
+        {"type": "divider"},
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"*どんな論文🧐？*\n hello"
+            }
+        }
+    ]
+
+    client.chat_postMessage(
+        channel=channel,
+        blocks=blocks,
+        text="今日のおすすめ論文をお届けします"
+    )
+
 if __name__ == "__main__":
-    keyword = 'Human activity recognition'
+    keyword = 'Human Activity recognition'
     venue = ('IEEE International Conference on Pervasive Computing and Communications',)
     # selected_paper = sp.research_paper(keyword, venue)
     # summary = gen.summarize(selected_paper)
     # print(summary)
     post_daily_paper(keyword, venue)
+    # check_ui()
