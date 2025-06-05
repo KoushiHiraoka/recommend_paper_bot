02c9eddcb319232db6eb49a779d6104f8b988b3e
KoushiHiraoka <hiraoka.koushi@arakawa-lab.com>
2025-06-05 12:29:07 +0900

    add: 漫画生成

--- regular_execution/recommend_bot.py
+++ regular_execution/recommend_bot.py
@@ -2,6 +2,7 @@ import os
 import datetime
 import requests
 import time
+import tempfile
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 from dotenv import load_dotenv
@@ -10,15 +11,14 @@ load_dotenv()
 import gen_intro as gen 
 import search_paper as sp
 
-# def get_summary(keyword, venue):
-#     selected_paper = sp.research_paper(keyword, venue)
-#     return gen.summarize(selected_paper)
+CLIENT = WebClient(token=os.getenv("SLACK_BOT_TOKEN"))
+CHANNELL =os.getenv("SLACK_CHANNEL") 
 
-def post_daily_paper(keyword, venue):
-    client = WebClient(token=os.getenv("SLACK_BOT_TOKEN"))
-    channel=os.getenv("SLACK_CHANNEL")
+def post_comic(keyword, venue, year_range):
 
-    selected_paper = sp.research_paper(keyword, venue)
+
+def post_daily_paper(keyword, venue, year_range):
+    selected_paper = sp.research_paper(keyword, venue, year_range)
     
     gpt_summary = gen.summarize(selected_paper)
     pop_title = gen.gen_title(selected_paper)
@@ -29,23 +29,6 @@ def post_daily_paper(keyword, venue):
     url      = selected_paper.get("url")
     authors  = ", ".join(a["name"] for a in selected_paper.get("authors", []))
     citation = selected_paper.get("citationCount")
-
-    ## v1 blocks 
-    # blocks = [
-    #     {"type":"header",
-    #      "text":{"type":"plain_text","text":"📚 今日のおすすめ論文 📚","emoji":True}},
-    #     {"type":"section",
-    #      "fields":[
-    #          {"type":"mrkdwn","text":f"*タイトル*\n<{url}|{title}>"},
-    #          {"type":"mrkdwn","text":f"*出版年*\n{year}"},
-    #          {"type":"mrkdwn","text":f"*会議*\n{venue}"},
-    #          {"type":"mrkdwn","text":f"*引用数*\n{citation}"},
-    #          {"type":"mrkdwn","text":f"*著者*\n{authors}"}
-    #      ]},
-        # {"type":"divider"},
-        # {"type":"section",
-        #  "text":{"type":"mrkdwn","text":f"*要旨*\n{summary_text}"}}
-    # ]
 
     blocks = [
         {
@@ -79,8 +62,8 @@ def post_daily_paper(keyword, venue):
         }
     ]
 
-    client.chat_postMessage(
-        channel=channel,
+    CLIENT.chat_postMessage(
+        channel=CHANNELL,
         blocks=blocks,
         text="今日のおすすめ論文をお届けします"  # フォールバックテキスト
     )
@@ -97,11 +80,43 @@ def post_daily_paper(keyword, venue):
     # except SlackApiError as err:
     #     print("Failed to post to Slack:", err)
 
+def post_image_url(keyword, venue, year_range):
+    selected_paper = sp.research_paper(keyword, venue, year_range)
+    
+    image_url = gen.generate_comic(selected_paper)
+
+    """
+    指定した image_url を Slack の channel に投稿する
+    """
+    try:
+        response = CLIENT.chat_postMessage(
+            channel=CHANNELL,
+            text="test",  # 画像だけ送りたいなら空文字でOK
+            blocks=[
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*どんな論文？🧐*"
+                    }
+                },
+                {"type": "divider"},
+                {
+                    "type": "image",
+                    "image_url": image_url,
+                    "alt_text": "4コマ漫画"
+                }
+            ]
+        )
+        print(f"画像を投稿しました (ts={response['ts']})")
+    except SlackApiError as e:
+        print(f"Slack API エラー: {e.response['error']}")
+
 if __name__ == "__main__":
     keyword = ' '
-    # venue = ('IEEE International Conference on Pervasive Computing and Communications','CHI')
-    venue = ('CHI',)
-    # selected_paper = sp.research_paper(keyword, venue)
-    # summary = gen.summarize(selected_paper)
-    # print(summary)
-    post_daily_paper(keyword, venue)
+    venue = ('Annual IEEE International Conference on Pervasive Computing and Communications','CHI')
+    year_range = 3
+    # post_daily_paper(keyword, venue, year_range)
+    # test_gen_comic(keyword, venue, year_range)
+    # image_url = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-ymJNkLG4S41rIZuoSnWOgVzi/user-FMHVKD6W1oVWCnUnsZAOYdzY/img-5MRDA8tdkTwzmFkMsOZGjvUW.png?st=2025-06-03T01%3A36%3A01Z&se=2025-06-03T03%3A36%3A01Z&sp=r&sv=2024-08-04&sr=b&rscd=inline&rsct=image/png&skoid=475fd488-6c59-44a5-9aa9-31c4db451bea&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-06-02T19%3A49%3A19Z&ske=2025-06-03T19%3A49%3A19Z&sks=b&skv=2024-08-04&sig=cI%2BZhZYz6XCE%2By%2BlhS4u8I86dilc2qyvJL0GBMALk2E%3D"
+    post_image_url(keyword, venue, year_range)
